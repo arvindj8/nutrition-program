e6ce529f234ee78ef2c2b95524b6e96d6d6d680d
Александр Буклов <arvindj8@gmail.com>
2023-04-20 21:34:04 +0300

    added spinner and working of sliders, also added cards menu from db.json

--- js/script.js
+++ js/script.js
@@ -43,7 +43,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Timer
 
-    const deadline = '2023-04-01'
+    const deadline = '2023-04-21'
 
     function getClock(endtime) {
         if (Date.parse(endtime) >= new Date()) {
@@ -78,7 +78,6 @@ window.addEventListener('DOMContentLoaded', () => {
             seconds = timer.querySelector('#seconds'),
             timeInterval = setInterval(updateClock, 1000)
 
-
         updateClock()
 
 
@@ -102,13 +101,17 @@ window.addEventListener('DOMContentLoaded', () => {
     // Modal
 
     const modalTrigger = document.querySelectorAll('[data-modal]'),
-        modal = document.querySelector('.modal'),
-        modalCloseAction = modal.querySelector('.modal__close')
+        modal = document.querySelector('.modal')
+
+    modalTrigger.forEach((btn) => {
+        btn.addEventListener('click', openModal)
+    })
 
     function openModal() {
-        modal.classList.toggle('show')
-        document.body.style.overflow = 'hidden'
-        clearTimeout(modalTimerId)
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
     }
 
     function closeModal() {
@@ -117,14 +120,9 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
 
-    modalTrigger.forEach((btn) => {
-        btn.addEventListener('click', openModal)
-    })
-
-    modalCloseAction.addEventListener('click', closeModal)
-
     modal.addEventListener('click', (event) => {
-        if (event.target === modal) {
+        if (event.target === modal ||
+            event.target.getAttribute('data-close') === '') {
             closeModal()
         }
     })
@@ -135,7 +133,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     })
 
-    const modalTimerId = setTimeout(openModal, 3000)
+    const modalTimerId = setTimeout(openModal, 50000)
 
 
     function openModalScroll() {
@@ -192,82 +190,147 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    new Card(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню “Фитнес”',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        6,
-        '.menu .container'
-    ).render()
+    const getDataCard = async (url) => {
+        const res = await fetch(url)
+        if (!res.ok) {
+            throw new Error(`Данные не получены, ошибка ${res.status}`)
+        }
+        return await res.json()
+    }
 
-    new Card(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        7,
-        '.menu .container'
-    ).render()
+    getDataCard('http://localhost:3000/menu')
+        .then(data => data.forEach(
+            ({img, altimg, title, descr, price}) => {
+            new Card(img, altimg, title, descr, price, '.menu .container')
+                .render()
+        }))
 
-    new Card(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        8,
-        '.menu .container'
-    ).render()
 
     // Form
 
     const forms = document.querySelectorAll('form')
 
     const message = {
-        loading: 'Загрузка',
+        loading: 'img/form/spinner.svg',
         success: 'Мы скоро свяжемся с вами.',
         failure: 'Что-то пошло не так'
     }
 
     forms.forEach(form => {
-        postData(form)
+        bindPostData(form)
     })
 
-    function postData(form) {
+    const postData = async (url, obj) => {
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: obj
+        })
+        return await res.json()
+    }
+
+    function bindPostData(form) {
         form.addEventListener('submit', (event) => {
             event.preventDefault()
-            const statusMessage = document.createElement('div')
-            statusMessage.classList.add('status')
-            statusMessage.textContent = message.loading
-            form.append(statusMessage)
+            const statusMessage = document.createElement('img')
+            statusMessage.src = message.loading
+            statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+            `
+            form.insertAdjacentElement("afterend", statusMessage)
 
-            const request = new XMLHttpRequest()
-
-            request.open('POST', 'server.php')
-            request.setRequestHeader('Content-type', 'application/json')
             const formData = new FormData(form)
 
-            const obj = {}
-            formData.forEach( (key, value) => {
-                obj[key] = value
-            })
+            // преобразуем formData в объект
+            const obj = Object.entries(Object.fromEntries(formData))
 
-            request.send(JSON.stringify(obj))
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    statusMessage.textContent = message.success
-                    form.reset()
-                    setTimeout(() => {
-                        statusMessage.remove()
-                    }, 2000)
-                } else {
-                    statusMessage.textContent = message.failure
-                }
+            postData('http://localhost:3000/requests', JSON.stringify(obj))
+            .then(data => {
+                console.log(data)
+                showThanksModal(message.success)
+                statusMessage.remove()
+            }).catch(() => {
+                showThanksModal(message.failure)
+            }).finally(() => {
+                form.reset()
             })
         })
     }
+
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector('.modal__dialog')
+
+        prevModalDialog.classList.add('hide')
+        openModal()
+
+        const thanksModal = document.createElement('div')
+        thanksModal.classList.add('modal__dialog')
+        thanksModal.innerHTML = `
+            <div class="modal__content">
+                <div class="modal__close" data-close>×</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `
+        document.querySelector('.modal').append(thanksModal)
+
+        setTimeout(() => {
+            thanksModal.remove()
+            prevModalDialog.classList.add('show');
+            prevModalDialog.classList.remove('hide');
+            closeModal()
+        }, 4000)
+    }
+
+    // Slider
+    let currentSlideIndex = 1
+
+    const slider = document.querySelector('.offer__slider'),
+        prevSlide = slider.querySelector('.offer__slider-prev'),
+        nextSlide = slider.querySelector('.offer__slider-next'),
+        currentSlide = slider.querySelector('#current'),
+        totalSlides = slider.querySelector('#total'),
+        slides = slider.querySelectorAll('.offer__slide')
+
+
+    showSlide(currentSlideIndex)
+
+
+    function showSlide(currentSlideIndex) {
+        currentSlide.textContent = addZeroDateTime(currentSlideIndex)
+        slides.forEach((slide, i= 0) => {
+            slide.classList.add('hide')
+
+            if (i === currentSlideIndex - 1) {
+                slide.classList.toggle('hide')
+                slide.classList.add('fade')
+            }
+        })
+    }
+
+    function changeSlide(slides) {
+        totalSlides.textContent = addZeroDateTime(slides.length)
+
+        nextSlide.addEventListener('click', () => {
+            if (currentSlideIndex <= slides.length - 1) {
+                currentSlideIndex += 1
+                showSlide(currentSlideIndex)
+            }
+        })
+
+        prevSlide.addEventListener('click', () => {
+            if (currentSlideIndex > 1) {
+                currentSlideIndex --
+                showSlide(currentSlideIndex)
+            }
+        })
+    }
+
+    changeSlide(slides)
 })
+
 
 
 
